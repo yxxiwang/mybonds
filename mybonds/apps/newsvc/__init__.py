e2e71a18a0f3eccf8a0227e46aa3c001f4a009f4
yxxiwang <yxxiwang@gmail.com>
2013-04-09 17:30:06 +0800

    修改了接口,恢复 摘要的结尾优化 功能

--- mybonds/apps/newsvc/__init__.py
+++ mybonds/apps/newsvc/__init__.py
@@ -50,8 +50,8 @@ def getDataByUrl(urlstr,isservice=False):
 #            doc["id"] = getHashid(doc["url"])
             doc["docid"] = getHashid(doc["url"])
             doc["title"] = doc["title"].replace(" ","")
-#             doc["tx"] = doc["text"].replace(" ","")
-#             doc["text"] = doc["text"]
+            doc["tx"] = doc["text"].replace(" ","")
+            doc["text"] = subDocText(doc["text"])
             doc["copyNum"] = str(doc["copyNum"])
             doc["validTime"] = str(doc["validTime"])
             doc["tms"]=str(doc["create_time"])
@@ -97,3 +97,27 @@ def getDocByUrl(urlstr):
         udata["total"] = "0"
         udata["docs"] = []
     return doc
+
+def subDocText(s):
+#    us=unicode(s,"utf8")
+#    return s
+    if s =="":
+        return s
+    s = s.replace(" ", "")
+#    .replace(",", "，".decode("utf8"))
+    us=to_unicode_or_bust(s)
+    lc=us[-1]
+    dot="。".decode("utf8")
+    comma = "，".decode("utf8")
+    ellipsis=" ......".decode("utf8")
+    if lc == dot or lc ==".": #从尾部判断，如果最后一个字符是"。"或者"." 则返回原始文本
+        return s
+    else:#否则开始进行截取
+        slst=us.split(dot)
+        if len(slst[-1]) <35:#如果最后一段在"。"之后文本长度小于35,则截断之
+            return dot.join(slst[0:-1]+[""]).encode("utf8")
+        else:#如果 最后一段文字数大于35个，则从尾部开始，截断到最近一个标点符合，包括，
+            clst=slst[-1].split(comma)
+            return (dot.join(slst[0:-1]+[""])+comma.join(clst[0:-1]+[""] ) ).encode("utf8")
+    return s
+        

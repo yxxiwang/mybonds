c23cb5f7ea85482cf57f46a64363a0f09a9ef3c1
yxxiwang <yxxiwang@gmail.com>
2013-04-23 10:17:13 +0800

    missing import re

--- mybonds/views.py
+++ mybonds/views.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
-import json
+import json,re
 import csv, string
 import sys, time
 import redis

529f23407069c1841a23f9d971d24dc2cc72c81d
yxxiwang <yxxiwang@gmail.com>
2013-10-30 18:51:46 +0800

    fixbugs

--- mybonds/apps/geeknews/views.py
+++ mybonds/apps/geeknews/views.py
@@ -564,21 +564,21 @@ def beaconsave(request, template_name="beacon_list.html"):
         from mybonds.build import beaconNameHash
         beaconNameHash()
     else:
-        if beaconkey != beaconusr + ":" + getHashid(beaconname):  # modifykeys
-#             print beaconkey,beaconusr+":"+getHashid(beaconname)
-            beaconid = getHashid(beaconname) 
-            beaconChangeName(beaconkey, beaconusr, beaconid)
-            key = "bmk:" + beaconusr + ":" + beaconid
-            r.hset(key, "id", beaconid)
-            r.hset(key, "crt_usr", beaconusr)
-            r.hset(key, "crt_tms", getUnixTimestamp(beacontime, "%Y%m%d%H%M%S")) 
-            r.hset(key, "ttl", beaconname)
-            r.hset(key, "name", beacondisplayname)
-            r.hset(key, "desc", desc)
-            r.hset(key, "mindoc", beaconmindoc) 
-            r.hset(key, "tag", beacontag)
-            r.zadd("bmk:doc:share", long(getUnixTimestamp(beacontime, "%Y%m%d%H%M%S")), beaconusr + "|-|" + beaconid)
-        else:  # modify desc and so on
+#         if beaconkey != beaconusr + ":" + getHashid(beaconname):  # modifykeys
+# #             print beaconkey,beaconusr+":"+getHashid(beaconname)
+#             beaconid = getHashid(beaconname) 
+#             beaconChangeName(beaconkey, beaconusr, beaconid)
+#             key = "bmk:" + beaconusr + ":" + beaconid
+#             r.hset(key, "id", beaconid)
+#             r.hset(key, "crt_usr", beaconusr)
+#             r.hset(key, "crt_tms", getUnixTimestamp(beacontime, "%Y%m%d%H%M%S")) 
+#             r.hset(key, "ttl", beaconname)
+#             r.hset(key, "name", beacondisplayname)
+#             r.hset(key, "desc", desc)
+#             r.hset(key, "mindoc", beaconmindoc) 
+#             r.hset(key, "tag", beacontag)
+#             r.zadd("bmk:doc:share", long(getUnixTimestamp(beacontime, "%Y%m%d%H%M%S")), beaconusr + "|-|" + beaconid)
+#         else:  # modify desc and so on
             r.hset(key, "crt_usr", beaconusr)
             r.hset(key, "crt_tms", getUnixTimestamp(beacontime, "%Y%m%d%H%M%S")) 
             r.hset(key, "desc", desc)

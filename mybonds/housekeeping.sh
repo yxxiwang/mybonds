03008c3b7b19df54ff10f56ab485344b602f48d0
yxxiwang <yxxiwang@gmail.com>
2014-01-28 23:54:25 +0800

    clean stock channel everyday

--- mybonds/housekeeping.sh
+++ mybonds/housekeeping.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 python /root/mybonds/build/__init__.py cleanDocChannelByTime 3 delete withstar
+python /root/mybonds/build/__init__.py cleanDocStockChannel doc delete
 redis-cli keys queue:* | grep done| xargs redis-cli del
  

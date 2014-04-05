49bb3d449b6e29051c26b12825868e1ec1a51941
yxxiwang <yxxiwang@gmail.com>
2014-04-05 12:05:59 +0800

    fixbugs

--- mybonds/build/__init__.py
+++ mybonds/build/__init__.py
@@ -124,7 +124,7 @@ def cleanChannelByCode(parms=("doc:1257408","print")):
         
         
 def updateChannelAndStock(parms=("24")):
-    urlstr = "http://%s/research/svc?hotstock=%d" % (getsysparm("BACKEND_DOMAIN"), parms[0])    
+    urlstr = "http://%s/research/svc?hotstock=%s" % (getsysparm("BACKEND_DOMAIN"), parms[0])    
     udata = loadFromUrl(urlstr) 
     udata.reverse()
     for rc in udata["docs"]:#{"channelName":"000002(万科Ａ)","channelId":"*000002(万科Ａ)","eventCreateTime":-1,"docId":-1,"docCreateTime":-1,"size":0}
@@ -133,7 +133,7 @@ def updateChannelAndStock(parms=("24")):
         addBeacon("stockmark", getHashid(rc["channelId"]), rc["channelName"], beaconname=beaconname, desc=rc["channelName"],tag="热门股票".decode("utf8"))
         r.hset("bmk:stockmark:" + getHashid(rc["channelId"]),"crt_tms",time.time())
         
-    urlstr = "http://%s/research/svc?hotconcept=%d" % (getsysparm("BACKEND_DOMAIN"), parms[0])    
+    urlstr = "http://%s/research/svc?hotconcept=%s" % (getsysparm("BACKEND_DOMAIN"), parms[0])    
     udata = loadFromUrl(urlstr) 
     udata.reverse()
     for rc in udata["docs"]:#{"channelName":"000002(万科Ａ)","channelId":"*000002(万科Ａ)","eventCreateTime":-1,"docId":-1,"docCreateTime":-1,"size":0}

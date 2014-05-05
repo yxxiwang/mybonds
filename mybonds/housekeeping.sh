0abd171f7633405c1221bcc541c7ec37190334ac
yxxiwang <yxxiwang@gmail.com>
2014-05-05 17:38:47 +0800

    save data

--- mybonds/housekeeping.sh
+++ mybonds/housekeeping.sh
@@ -4,4 +4,6 @@ python /root/mybonds/build/__init__.py cleanDocChannelByTime 1 delete notwithtag
 python /root/mybonds/build/__init__.py updateChannelAndStock 24
 
 redis-cli keys queue:* | grep done| xargs redis-cli del
- 
+
+redis-cli save
+cp /data/dump.rdb  /data/dump.rdb.$(date +'%Y%m%d')
